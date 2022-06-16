03537992c90f4d7675cbc85c07d4839f6a82de0a
Charlie-Moon <chanthailyios@gmail.com>
2022-06-16 13:18:32 +0700

    fix card

--- resources/views/front/partials/product.blade.php
+++ resources/views/front/partials/product.blade.php
@@ -1,7 +1,9 @@
 @foreach ($product as $item)
 		<div class="col">
 			<a href="/products/{{ $item->id }}" class="card">
-				<img class="card-img" src="{{ url('/').'/public/Image/'.$item->serviceImage }}" alt="{{ $item->serviceTitle }}">
+				<div class="ratio ratio-16x9">
+					<img class="card-img" src="{{ url('/').'/public/Image/'.$item->serviceImage }}" alt="{{ $item->serviceTitle }}">
+				</div>
 				<div class="card-img-overlay">
 					<h5 class="card-title">{{ $item->serviceTitle }}</h5>
 					<p class="card-text line-clamp-3">{{ $item->serviceExcerpt }}</p>
